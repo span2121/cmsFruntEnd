8f9c6fa599d3c75653ce32bdae9c356d66ae5bcc
Spandana <spanspan299@gmail.com>
2021-03-23 21:38:02 +0530

    database connect

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,20 +1,21 @@
 'use strict';
 const mysql = require('mysql');
 //local mysql db connection
+
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'Activityclg'
+  host     : 'us-cdbr-east-03.cleardb.com',
+  user     : 'b9a579c066c741',
+  password : '74fb8269',
+  database : 'heroku_746c5166d3a7b08'
 });
-// to be contineous interaction with server
-// setInterval(function () {
-//   dbConn.query('select * from user');
-// }, 5000);
-// dbConn.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Database Connected!");
-// });
+setInterval(function () {
+  dbConn.query('select * from user');
+}, 5000);
+dbConn.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+
 module.exports = dbConn;
 // for database connection 
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
