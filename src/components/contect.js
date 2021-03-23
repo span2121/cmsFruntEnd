7c61a99dd77393bc6e5d7f3eee24c3a40aa287d9
Spandana <spanspan299@gmail.com>
2021-03-24 00:55:29 +0530

    change API Path

--- src/components/contect.js
+++ src/components/contect.js
@@ -1,8 +1,7 @@
 import { createContext } from  'react' ;
 
 const AppContext = createContext({
-    API_PATH: "http://localhost:5000/api",
-    //API_PATH:"https://backendcms.herokuapp.com/api"
-
+    //API_PATH: "http://localhost:5000/api",
+    API_PATH:"https://backendcms.herokuapp.com/api"
 })
 export  default AppContext;
