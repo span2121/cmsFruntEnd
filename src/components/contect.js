be71afbb54ee3d0eb2262f0b1b4ec52644ce00ee
Spandana <spanspan299@gmail.com>
2021-03-26 09:05:44 +0530

    api call issue

--- src/components/contect.js
+++ src/components/contect.js
@@ -1,7 +1,7 @@
 import { createContext } from  'react' ;
 
 const AppContext = createContext({
-    API_PATH: "http://localhost:5001/api",
-    //API_PATH:"https://backendcms.herokuapp.com/api"
+    //API_PATH: "http://localhost:5001/api",
+    API_PATH:"https://backendcms.herokuapp.com/api"
 })
 export  default AppContext;
